d118393bc5a9d997718ed5b8178544946b66c084
Christee <ra2847545015@gmail.com>
2023-01-13 18:56:18 +0800

    update

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 import discord
 from discord.ext import commands
+import requests
 import os
 import re
+import json
 
 
 # client 是我們與 Discord 連結的橋樑，intents 是我們要求的權限
 intents = discord.Intents.default()
 intents.message_content = True
 client = commands.Bot(command_prefix='$', intents=intents)
+
+# 馬哥吃草
+@client.command(name='馬哥')
+async def _list(ctx):
+    await ctx.send("馬哥吃草")
+
+# 隨機本子
+@client.command()
+async def random(ctx):
+    await ctx.send("https://nhentai.net/random")
+
+# 唬爛造句
+@client.command()
+async def hulan(ctx, topic, len):
+    try:
+        r = requests.get("https://fastapi-selenium-production-8ccc.up.railway.app/hulan?topic={topic}&len={len}")
+        data = json.loads(r.text)
+        await ctx.send(data['text'])
+    except:
+        await ctx.send("something went wrong.")
 
 @client.event
 async def on_ready():
@@ -26,10 +48,4 @@ async def on_message(message):
         if len(num) == 6:
             await message.channel.send('https://nhentai.net/g/' + num)
 
-    # 隨機本子
-    if message.content.startswith('$random'):
-        await message.channel.send('https://nhentai.net/random')
-
-
-client.change_presence(status=discord.Status.idle, activity="reading papers")
 client.run(os.environ["DISCORD_TOKEN"]) 
