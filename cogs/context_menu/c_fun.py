7328282d1ccd70785fb969d8dec86a3a60cbaea8
Christee <ra2847545015@gmail.com>
2024-05-31 12:45:57 +0800

    Debug: c_fun.py

--- cogs/context_menu/c_fun.py
+++ cogs/context_menu/c_fun.py
@@ -21,27 +21,31 @@ class CFun(commands.Cog):
         self.bot.tree.add_command(self.ctx_menu)
 
     async def quote(self, interaction: discord.Interaction, message: discord.Message) -> None:
+        await interaction.response.defer()  # 延遲互動回應以延長時間限制
+
         if message.attachments:
             for attachment in message.attachments:
                 if attachment.content_type.startswith('image/'):
                     encoded_url = quote(attachment.url, safe='') 
                     try:
                         r = requests.get(settings["root"] + f"getImageHentai?url={encoded_url}")
+                        r.raise_for_status()
                         data = json.loads(r.text)
                         
                     except RequestException as e:
                         print(f"請求失敗: {e}")
+                        await interaction.followup.send("處理圖片失敗。")
                         data = None
 
                     embed=discord.Embed(title="馬哥の草", color=0x118845)
                     for index, i in enumerate(data):
                         embed.add_field(name=str(index+1)+". "+i["title"], value=i["src"], inline=False)
-                    embed.set_thumbnail(url="https://i.imgur.com/5cDx2fG.png")
+                    embed.set_thumbnail(url="https://i.imgur.com/HtLNVb8.png")
                     embed.set_footer(text="NTRの戦士")
-                    await interaction.response.send_message(embed=embed)
+                    await interaction.followup.send(embed=embed)
             return
-                  
-        await interaction.response.send_message("It's not a image.")
+        
+        await interaction.followup.send("It's not a image.")
 
 
 async def setup(bot):
